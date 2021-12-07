582386f51e9a66b13f89960154c4c2b226bca329
Christine Yang <christine.yang@yale.edu>
2021-12-07 18:24:19 -0500

    init chord rnn

--- generate_music.js
+++ generate_music.js
@@ -84,18 +84,22 @@ const presetMelodies = {
   },
 }
 
-
 const twinkleButton = document.getElementById('twinkle')
 const arpeggiatedButton = document.getElementById('arpeggiated')
 const sparseButton = document.getElementById('sparse')
 
 const playLoadedMusicButton = document.getElementById('loadedMusic')
 
+
+const playNoteSeqFromChord = document.getElementById('noteSeqFromChord')
+
 twinkleButton.onclick = () => { this.player.start(presetMelodies.Twinkle) }
 
 arpeggiatedButton.onclick = () => { this.player.start(presetMelodies.Arpeggiated) }
 
 sparseButton.onclick = () => { this.player.start(presetMelodies.Sparse) }
+
+playNoteSeqFromChord.onclick = () => { this.player.start(this.noteSequenceFromChord) }
 
 playLoadedMusicButton.onclick = () => {
   this.player.start(this.noteSequence)
@@ -112,14 +116,15 @@ window.onload = () => {
   initRNN()
 
   // TODO: init chord cnn, make this a option
+  initChordRNN()
 };
+
+const nOfBars = 8 // hardcode
 
 const initRNN = () => {
   console.log('hi')
   const modelCheckPoint = 'https://storage.googleapis.com/magentadata/js/checkpoints/music_rnn/basic_rnn'
   const model = new mm.MusicRNN(modelCheckPoint);
-
-  const nOfBars = 8 // hardcode
 
   console.log('here')
 
@@ -139,3 +144,132 @@ const initRNN = () => {
       this.model = model;
     });
 }
+
+const changeChordsButton = document.getElementById('changeChords');
+const chordsContainer = document.getElementById('chordsContainer');
+const statusDiv = document.getElementById('changeChordsPopup');
+const chordInputs = [
+  document.getElementById('chord1'),
+  document.getElementById('chord2'),
+  document.getElementById('chord3'),
+  document.getElementById('chord4')
+];
+
+const numChords = chordInputs.length;
+var chordSeqs;
+
+
+var changingChords = false;
+
+changeChordsButton.onclick = toggleChangeChords;
+
+chordInputs.forEach(c => c.oninput = chordChanged);
+
+var chords = chordInputs.map(c => c.value);
+
+// Set UI state to changing chords.
+function setChordChangeState() {
+    statusDiv.innerText = 'Change chords (letters A through G, optionally followed by "m", "dim", "aug" for minor, diminished, and augmented chords), then press Done.';
+    changeChordsButton.innerText = 'Done';
+    chordsContainer.removeAttribute('disabled');
+    chordInputs.forEach(c => c.classList.remove('playing'));
+  }
+  
+function setUpdatingState() {
+    statusDiv.innerText = '';
+    chordsContainer.setAttribute('disabled', true);
+    changeChordsButton.innerText = 'Change';
+  }
+  
+// Start or finish changing chords.
+function toggleChangeChords() {
+    if (changingChords) {
+      changingChords = false;
+      chords = chordInputs.map(c => c.value);
+      const modelCheckPoint = 'https://storage.googleapis.com/magentadata/js/checkpoints/music_rnn/chord_pitches_improv'
+      const model = new mm.MusicRNN(modelCheckPoint);
+      console.log(chords);
+      initChordRNN()
+      // model.initialize()
+      // .then(() => {
+      //   console.log('initialized chord rnn!');
+      //   return model.continueSequence(
+      //     presetMelodies.Arpeggiated,
+      //     nOfBars * 16,
+      //     1.0, chords)
+      // })
+      // .then((noteSequence) => {
+      //   // this.setMelodies([i]);
+      //   console.log("new" + noteSequence);
+  
+      //   this.noteSequence = noteSequence;
+      //   this.model = model;
+      // });
+      setUpdatingState();
+    //   setTimeout(() => generateProgressions(setStoppedState), 0);
+    } else {
+      playing = false;
+      changingChords = true;
+      setChordChangeState();
+    //   player.stop();
+    }
+  }
+
+// One of the chords has been edited.
+function chordChanged() {
+    const isGood = (chord) => {
+      if (!chord) {
+        return false;
+      }
+      try {
+        mm.chords.ChordSymbols.pitches(chord);
+        return true;
+      } catch(e) {
+        return false;
+      }
+    }
+    
+    var allGood = true;
+    chordInputs.forEach(c => {
+      if (isGood(c.value)) {
+        c.classList.remove('invalid');
+      } else {
+        c.classList.add('invalid');
+        allGood = false;
+      }
+    });
+  
+    changeChordsButton.disabled = !allGood;
+    // if (allGood)
+    // {
+    //     console.log(chords);
+    //     // chordInputs.forEach(c => {
+    //     //     console.log(c.value);
+    //     // })
+    // }
+    
+  }
+  
+  const initChordRNN = () => {
+    console.log('hi')
+    const modelCheckPoint = 'https://storage.googleapis.com/magentadata/js/checkpoints/music_rnn/chord_pitches_improv'
+    const model = new mm.MusicRNN(modelCheckPoint);
+  
+    console.log('here')
+  
+    model.initialize()
+      .then(() => {
+        console.log('initialized non-chord rnn!');
+        return model.continueSequence(
+          presetMelodies.Arpeggiated,
+          nOfBars * 16,
+          1.0, chords)
+      })
+      .then((noteSequence) => {
+        // this.setMelodies([i]);
+        console.log(noteSequence)
+  
+        this.noteSequenceFromChord = noteSequence
+        this.model = model;
+      });
+  }

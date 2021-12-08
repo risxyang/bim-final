a416cdfea77f568d004f9a84d709fd371a1f4d91
Christine Yang <christine.yang@yale.edu>
2021-12-07 22:18:22 -0500

    piano!

--- generate_music.js
+++ generate_music.js
@@ -87,23 +87,20 @@ const presetMelodies = {
 const twinkleButton = document.getElementById('twinkle')
 const arpeggiatedButton = document.getElementById('arpeggiated')
 const sparseButton = document.getElementById('sparse')
-
 const playLoadedMusicButton = document.getElementById('loadedMusic')
-
-
 const playNoteSeqFromChord = document.getElementById('noteSeqFromChord')
+const originalNoteSeqButton = document.getElementById('originalSequence')
+const finishedPlayingButton = document.getElementById('finishedPlaying')
+const piano = document.getElementById('pianoContainer')
 
 twinkleButton.onclick = () => { this.player.start(presetMelodies.Twinkle) }
-
 arpeggiatedButton.onclick = () => { this.player.start(presetMelodies.Arpeggiated) }
-
 sparseButton.onclick = () => { this.player.start(presetMelodies.Sparse) }
-
 playNoteSeqFromChord.onclick = () => { this.player.start(this.noteSequenceFromChord) }
+playLoadedMusicButton.onclick = () => {this.player.start(this.noteSequence)}
+originalNoteSeqButton.onclick = () => { rolloutPiano() }
+finishedPlayingButton.onclick = () => { hidePiano() }
 
-playLoadedMusicButton.onclick = () => {
-  this.player.start(this.noteSequence)
-}
 
 window.onload = () => {
   console.log('window.onload!')
@@ -273,3 +270,125 @@ function chordChanged() {
         this.model = model;
       });
   }
+
+  /*PIANO*/
+
+  const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm']
+const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j']
+
+const recordButton = document.querySelector('.record-button')
+const playButton = document.querySelector('.play-button')
+const saveButton = document.querySelector('.save-button')
+const songLink = document.querySelector('.song-link')
+const keys = document.querySelectorAll('.key')
+const whiteKeys = document.querySelectorAll('.key.white')
+const blackKeys = document.querySelectorAll('.key.black')
+
+const keyMap = [...keys].reduce((map, key) => {
+  map[key.dataset.note] = key
+  return map
+}, {})
+
+let recordingStartTime
+let songNotes = currentSong && currentSong.notes
+
+// TODO: add a clear() function to clear the current song notes 
+
+keys.forEach(key => {
+  key.addEventListener('click', () => playNote(key))
+})
+
+if (recordButton) {
+  recordButton.addEventListener('click', toggleRecording)
+}
+if (saveButton) {
+  saveButton.addEventListener('click', saveSong)
+}
+playButton.addEventListener('click', playSong)
+
+document.addEventListener('keydown', e => {
+  if (e.repeat) return
+  const key = e.key
+  const whiteKeyIndex = WHITE_KEYS.indexOf(key)
+  const blackKeyIndex = BLACK_KEYS.indexOf(key)
+
+  if (whiteKeyIndex > -1) playNote(whiteKeys[whiteKeyIndex])
+  if (blackKeyIndex > -1) playNote(blackKeys[blackKeyIndex])
+})
+
+function toggleRecording() {
+  recordButton.classList.toggle('active')
+  if (isRecording()) {
+    startRecording()
+  } else {
+    stopRecording()
+  }
+}
+
+function isRecording() {
+  return recordButton != null && recordButton.classList.contains('active')
+}
+
+function startRecording() {
+  recordingStartTime = Date.now()
+  songNotes = []
+  playButton.classList.remove('show')
+  saveButton.classList.remove('show')
+}
+
+function stopRecording() {
+  playSong()
+  playButton.classList.add('show')
+  saveButton.classList.add('show')
+}
+
+function playSong() {
+  if (songNotes.length === 0) return
+  songNotes.forEach(note => {
+    setTimeout(() => {
+      playNote(keyMap[note.key])
+    }, note.startTime)
+  })
+}
+
+function playNote(key) {
+  if (isRecording()) recordNote(key.dataset.note)
+  const noteAudio = document.getElementById(key.dataset.note)
+  noteAudio.currentTime = 0
+  noteAudio.play()
+  key.classList.add('active')
+  noteAudio.addEventListener('ended', () => {
+    key.classList.remove('active')
+  })
+}
+
+function recordNote(note) {
+  songNotes.push({
+    key: note,
+    startTime: Date.now() - recordingStartTime
+  })
+}
+
+function saveSong() {
+  axios.post('/songs', { songNotes: songNotes }).then(res => {
+    songLink.classList.add('show')
+    songLink.href = `/songs/${res.data._id}`
+  })
+}
+
+
+
+function rolloutPiano()
+{
+  console.log("show piano");
+  piano.removeAttribute("hidden"); 
+
+}
+
+function hidePiano()
+{
+  console.log("hide piano");
+  piano.setAttribute("hidden",""); 
+  console.log("song:" + songNotes)
+
+}

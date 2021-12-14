26c043a663c805c39bd6f9fefdd435ead4fc13bc
Robert L <1998.robert@gmail.com>
2021-12-14 01:19:23 -0500

    prettier
    prettier css
    
    prettier
    
    prettier

--- display.js
+++ display.js
@@ -4,15 +4,17 @@ const NS2Button = document.getElementById('customNS')
 const NSOptions = document.getElementById('preloadedOptions')
 const pianoContainer = document.getElementById('pianoContainer')
 
-NS1Button.onclick = () => { 
-    console.log("show options");
-    pianoContainer.setAttribute("hidden", "");
-    NSOptions.removeAttribute("hidden"); }
+NS1Button.onclick = () => {
+  console.log("show options");
+  pianoContainer.setAttribute("hidden", "");
+  NSOptions.removeAttribute("hidden");
+}
 
-NS2Button.onclick = () => { 
-    console.log("show piano");
-    NSOptions.setAttribute("hidden", "");
-    pianoContainer.removeAttribute("hidden"); }
+NS2Button.onclick = () => {
+  console.log("show piano");
+  NSOptions.setAttribute("hidden", "");
+  pianoContainer.removeAttribute("hidden");
+}
 
 //Menu Setup Part 2
 
@@ -21,23 +23,25 @@ const noChordsButton = document.getElementById('noChords')
 const chordSelector = document.getElementById('chordSelector')
 const noChordSelector = document.getElementById('noChordsSelector')
 
-noChordsButton.onclick = () => { 
-    console.log("hide chords");
-    chordSelector.setAttribute("hidden", "");
-    noChordSelector.removeAttribute("hidden");}
+noChordsButton.onclick = () => {
+  console.log("hide chords");
+  chordSelector.setAttribute("hidden", "");
+  noChordSelector.removeAttribute("hidden");
+}
 
-inputChordsButton.onclick = () => { 
-    console.log("show chords");
-    chordSelector.removeAttribute("hidden");
-    noChordSelector.setAttribute("hidden", "");}
+inputChordsButton.onclick = () => {
+  console.log("show chords");
+  chordSelector.removeAttribute("hidden");
+  noChordSelector.setAttribute("hidden", "");
+}
 
 //navigation
 
 const continueButton1 = document.getElementById('continue1')
 const section2 = document.getElementById('settings')
 continueButton1.onclick = () => {
-    section2.scrollIntoView();
- }
+  section2.scrollIntoView();
+}
 
 
 // const continueButton2 = document.getElementById('finishedPlaying')
